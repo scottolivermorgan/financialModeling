5421db1fc5c8267e02ef2952fd11ccb84d102990
Scott Morgan <scottolivermorgan@yahoo.com>
2023-08-02 19:45:24 +0100

    Corrected plot dating.

--- helpers/functions.py
+++ helpers/functions.py
@@ -1,23 +1,38 @@
-from datetime import datetime
+import datetime as dt
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy import stats
 from scipy.stats import norm
+import os
+
+# TODO:
+#   verify slope gradient - looks incorrect when using dates
+
+OUTPUT_PATH = 'output//'
+
+def output_dir(name):
+    """Create sub-dir to store plots if it dosen't exist
+    Args:
+        name -- Sub directory name.
+    Returns:
+        none    
+    """
+    if not os.path.isdir(f'{OUTPUT_PATH}{name}'):
+        # directory dose not exist, create it
+        os.mkdir(f'{OUTPUT_PATH}{name}')
+    return f'{OUTPUT_PATH}{name}//'
 
 def getData(ticker):
     """Retrive financial data for asset from yahoo finance API
-
     Args:
         ticker -- Asset ticker as string.
-    
     Returns:
         df -- Pandas dataframe
     """
-
     # todays date
-    current_dateTime = datetime.now()
+    current_dateTime = dt.datetime.now()
     
     # Retrive info
     asset = yf.Ticker(ticker)
@@ -39,47 +54,53 @@ def getData(ticker):
 
 def price_vs_time(df, name, log):
     """Plot asset price or logged asset price over time.
-
     Args:
         df -- Pandas dataframe.
         name -- Asset ticker as string.
         log -- Boolean
-    
     Returns:
         plt -- Matplotlib plot object
+    TODO:
+        return plot instead of show.
     """
-    x = df.index#df['timestamp']
+    x = df['timestamp']
     start_date = x.min()
     end_date = x.max()
     plt.figure(1, [10,5])
     if log == True:
+        # Convert dates ton unixtime for fitting
+        x = (df['timestamp'] - dt.datetime(1970,1,1)).dt.total_seconds()
         y = df['Log Close']
+
+        # Create linear fit
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+        y_fit = slope*x + intercept
+        fit_details = f"f(y) = {str(slope)}x + {str(round(intercept,4))}"
+
+        # Plotting details
         plt.title(f'{name}  Log($) vs Time')
-        slope, intercept, r_value, p_value, std_err = stats.linregress(df.index, y)
-        fit_details = f"f(y) = {str(round(slope, 4))}x + {str(round(intercept,4))}"
-        x_fit = np.linspace(0, len(x), num = len(x))
-        y_fit = slope*x_fit + intercept
-        plt.plot(x_fit, y_fit, 'b-', label = fit_details)
-        plt.plot(x, y, 'k-')
+        plt.plot(df['timestamp'], y_fit, 'b-', label = fit_details)
+        plt.plot(df['timestamp'], y, 'k-')
         plt.ylabel('Log Price ($)', fontsize = 20)
         plt.xlabel('Date', fontsize = 20)
-    
         plt.xlim(start_date, end_date)
         plt.legend()
-        plt.savefig(f'output//Log({name}).png')
+        path = output_dir(name)
+        plt.savefig(f'{path}Log({name}).png')
 
     else:
-        #x = df['timestamp']
+        x = df['timestamp']
         y = df['Open']
+
+        # Plotting details
         plt.plot(x, y, 'k-')
         plt.ylabel('Price ($)', fontsize = 20)
         plt.title(f'{name} $ vs Time')
         plt.xlabel('Date', fontsize = 20)
-    
         plt.xlim(start_date, end_date)
-        plt.savefig(f'output//{name}.png')
-    
-    plt.show()
+        path = output_dir(name)
+        plt.savefig(f'{path}{name}.png') 
+    #plt.show()
 
 
 def largestDrawDown(df):
@@ -91,7 +112,6 @@ def largestDrawDown(df):
         df_transform -- Pandas dataframe of all drawdowns sorted by magnitude.
     
     """
-
     df_transform = pd.DataFrame(columns = [
                                     'Open',
                                     'High',      
@@ -107,7 +127,6 @@ def largestDrawDown(df):
                                     'Max Price',
                                     'Max Drawdown'
                                     ])
-
 
     for index, row in df.iterrows():
         # return all values greater than start date with a lower price
@@ -135,12 +154,18 @@ def largestDrawDown(df):
                                         })
 
         df_transform = pd.concat([df_transform, temp_df2])
-        #var = df_transform.sort_values(by=['Max Drawdown'], ascending=[False])
     return df_transform
 
 def distribution_plot(df, name):
-    # Histogram of daily percentage movments
-
+    """ Return histogram of daily percentage movments
+    Args:
+        df   -- Pandas dataframe
+        name -- Assets ticker name
+    Return:
+        None
+    TODO:
+        Return plot instead of show.
+    """
     percentSet=(df['Percentage']-1.)*100.
     #Plot params
     fig= plt.figure(1,[10,10])
@@ -148,16 +173,18 @@ def distribution_plot(df, name):
     ax.tick_params(axis='both', which='major', labelsize=20)
     var= ax.hist(percentSet,bins=200)
     plotHeight=sorted(var[0])
-    #plotWidth=sorted(var[1])
+
     plt.xlabel('Daily movment (%)',fontsize=20)
     plt.ylabel('Frequency',fontsize=20)
     plt.ylim(plotHeight[0],plotHeight[-1])
     plt.title(name,fontsize=40)
+
     # Stats
     mean,std=norm.fit(percentSet)
     std1=[std,std] ;std2=[2*std,2*std];std3=[3*std,3*std]
     Nstd1=[(-std+mean),(-std+mean)] ;Nstd2=[2*(-std+mean),2*(-std+mean)];Nstd3=[3*(-std+mean),3*(-std+mean)]
     aveX=[mean,mean]; aveY=[0,plotHeight[-1]+5]
+
     #plot and save
     plt.plot(aveX,aveY,'k--',linewidth=2)
     plt.plot(std1,aveY,'r:',linewidth=2)
@@ -168,14 +195,8 @@ def distribution_plot(df, name):
     plt.plot(Nstd3,aveY,'r:',linewidth=2)
     m='Mean ='+str(round(mean,4))+'%'
     s='STD ='+str(round(std,4))+'%'
-    #plt.text(loc1[0],loc1[1],m,fontsize=20)
     plt.tight_layout()
-    #plt.text(loc2[0],loc2[1],s,fontsize=20)
-    plt.savefig('output//'+name+'Distribution'+'.png',bbox_inches="tight")
-    plt.show()
-    
 
-
-    
-
-    
+    path = output_dir(name)
+    plt.savefig(f'{path}'+name+'Distribution'+'.png',bbox_inches="tight")
+    #plt.show()
